2e260b07b22f107c455c21d864c766fe920086d6
Vishnu <w0834097@myscc.ca>
2024-12-02 23:48:06 +0530

    tried to do something but it did'nt work.

--- src/main/java/com/example/housepos/tabs/RemoveItem.java
+++ src/main/java/com/example/housepos/tabs/RemoveItem.java
@@ -1,7 +1,10 @@
 package com.example.housepos.tabs;
 
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class RemoveItem extends Tab {
 
@@ -9,6 +12,11 @@ public class RemoveItem extends Tab {
 
     public RemoveItem() {
         this.setText("Remove Item");
+        BorderPane root = new BorderPane();
+
+        // Create form elements for removing a new property
+        VBox form = new VBox(10); // 10 is the spacing between elements
+        form.setPadding(new Insets(10, 10, 10, 10));
     }
 
     public static RemoveItem getInstance() {
